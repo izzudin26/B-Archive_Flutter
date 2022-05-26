0150a9703f3d6b2968e9ecfdc6a0c2a785cfff6c
izzudin26 <izzudin.ar26@gmail.com>
2022-05-27 04:00:00 +0700

    add splash

---
+++ lib/screen/splash.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:b_archive/service/auth.dart' as _auth;
+import 'mainmenu.dart';
+import 'login.dart';
+
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 2), () async {
+      await fetchToken();
+    });
+  }
+
+  Future<void> fetchToken() async {
+    String? token = await _auth.getToken();
+    if (token != null) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MainMenu()));
+    }
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Login()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Container(
+              padding: EdgeInsets.all(20),
+              child: Lottie.asset('assets/splashloading.json'))),
+    );
+  }
+}

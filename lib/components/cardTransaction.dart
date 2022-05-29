c7f09a09bd524331774c368ec5ea45f01d9622de
izzudin26 <izzudin.ar26@gmail.com>
2022-05-30 02:50:17 +0700

    add transaction card component

---
+++ lib/components/cardTransaction.dart
@@ -0,0 +1,61 @@
+import 'package:b_archive/screen/formTransactionView.dart';
+import 'package:flutter/material.dart';
+import 'package:b_archive/model/blockdata.dart';
+import 'package:b_archive/style/style.dart' as style;
+
+class CardTransaction extends StatelessWidget {
+  Metadata metadata;
+  CardTransaction({Key? key, required this.metadata}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => FormTransactionView(metadata: metadata)));
+      },
+      child: Container(
+        padding: EdgeInsets.all(15),
+        margin: EdgeInsets.only(top: 10, bottom: 10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Theme.of(context).colorScheme.primary),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  metadata.transactionType.toUpperCase(),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32),
+                )
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 5),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(metadata.receiverName,
+                        style: style.subtitleCardTransaction()),
+                    Text(metadata.receiverNumber,
+                        style: style.subtitleCardTransaction()),
+                    Text(metadata.referenceNumber,
+                        style: style.subtitleCardTransaction()),
+                    Text(metadata.transactionDate,
+                        style: style.subtitleCardTransaction())
+                  ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
